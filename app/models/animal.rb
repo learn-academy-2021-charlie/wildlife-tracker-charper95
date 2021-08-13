11b77febd9975c8133a331837f30741c1ddee090
charper95 <charper1995@gmail.com>
2021-08-12 18:03:54 -0700

    finished last two basic stretch challenges

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,4 +4,11 @@ class Animal < ApplicationRecord
     validates :latin_name, presence: true
     validates :common_name, uniqueness: true
     validates :latin_name, uniqueness: true
+    validate :name_checker
+    def name_checker
+        if common_name == latin_name
+            errors.add(:common_name, 'cant be the same as latin_name')
+        end
+    end
 end
+
